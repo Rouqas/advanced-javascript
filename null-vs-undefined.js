7506ed9757f0d11d21484f4d563479d5f4ce900b
Newton Nath <newtonnath07@gmail.com>
2021-02-15 19:07:47 +0000

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1 , num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+// const result = add(13, 45);
+// console.log(result)
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+// const result = add(20);
+
+// const premik = {name: 'shahin', phone: 568};
+// console.log(premik.height)
+
+// const num = [33, 66, 77];
+// console.log(num[9]);
+
+const fun = undefined;
+console.log(fun)
