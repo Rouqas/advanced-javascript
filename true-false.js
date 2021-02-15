a11eac1a91f814de46bd9777278dbb8d0cf4cbb5
Newton Nath <newtonnath07@gmail.com>
2021-02-15 18:00:45 +0000

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// truthy:
+// " ","0"
+// []
+// {}
+// "false"
+
+let name = false;
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
